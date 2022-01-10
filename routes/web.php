8882323c4ca36257554f2682b19a2824bde4dbe9
davidfactis <factis452006@gmail.com>
2022-01-09 19:51:47 -0800

    middleware login

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/','App\Http\Controllers\controllerLogin@autho');
+Route::get('/','App\Http\Controllers\controllerLogin@autho')->middleware('auth');
